c230b74e715fcf13bb89e720503d8a8b2f5bdf0d
robbryandev <robbryandev@gmail.com>
2023-10-02 16:49:14 -0700

    Implement NativeWind

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="text-center p-4 pt-16">
+      <Text className="font-semibold text-2xl">Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

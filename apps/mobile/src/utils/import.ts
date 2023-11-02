70f925c6cd932059c79aef538a8fb53b6330a98c
robbryandev <robbryandev@gmail.com>
2023-11-02 16:09:23 -0700

    Fix google import secret encoding

--- apps/mobile/src/utils/import.ts
+++ apps/mobile/src/utils/import.ts
@@ -28,6 +28,32 @@ export function isMigration(url: string) {
   return rx.test(url);
 }
 
+function isBase32(val: string): Boolean {
+  const b32 = /^[A-Z2-7]+=*$/;
+  return b32.test(val);
+}
+
+function base64ToBase32(base64: string) {
+  const base32Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
+  const bytes = Buffer.from(base64, 'base64');
+  let binaryString = '';
+
+  for (let i = 0; i < bytes.length; i++) {
+    binaryString += bytes[i].toString(2).padStart(8, '0');
+  }
+
+  let base32String = '';
+
+  for (let i = 0; i < binaryString.length; i += 5) {
+    const chunk = binaryString.substring(i, i + 5).padEnd(5, "0");
+    const index = parseInt(chunk, 2);
+    base32String += base32Chars[index];
+  }
+
+  return base32String;
+}
+
 export async function decodeMigration(url: string): Promise<GoogleExports> {
   try {
     // Extract the Base64-encoded data from the URL
@@ -55,9 +81,16 @@ export async function decodeMigration(url: string): Promise<GoogleExports> {
       longs: String,
       enums: String,
       bytes: String,
-    });
+    }) as GoogleExports;
 
-    return decodedDataJson as GoogleExports;
+    decodedDataJson.otpParameters = decodedDataJson.otpParameters.map((newCode: GoogleCode) => {
+      const tmpCode: GoogleCode = { ...newCode };
+      if (!isBase32(tmpCode.secret)) {
+        tmpCode.secret = base64ToBase32(tmpCode.secret);
+      }
+      return tmpCode;
+    });
+    return decodedDataJson;
   } catch (error) {
     console.error('Error decoding GoogleAuthenticatorImport:', error);
     throw error;
